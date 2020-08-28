1ff50d5c3d04a9ff8f54216c1157762c223c01b8
afdhalluthfi09 <afdhal.luthfi@students.amikom.ac.id>
2020-08-28 13:45:39 +0700

    saya baru komit

--- novice/02-04/kasus/asyc_syc/index.js
+++ novice/02-04/kasus/asyc_syc/index.js
@@ -6,13 +6,29 @@
 //   console.log(body.explanation);
 // });
 
-const axios = require('axios');
+// const axios = require('axios');
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  .then(response => {
-    console.log(response.data.url);
-    console.log(response.data.explanation);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+//   .then(response => {
+//     console.log(response.data.url);
+//     console.log(response.data.explanation);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// const http =require('http');
+
+// const handleServer =(request,response)=>{
+//     response.writeHead(200,{
+//         'Content-Type' : 'text/html'
+//     })
+//     response.end('hello word')
+// }
+// http.createServer(handleServer).listen(5432,'127.0.0.1')
+
+// console.log('coba cliki http://127.0.0.1:5432')
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
